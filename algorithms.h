342954634062eca361e4cc424df32b125b0e1455
osbins <arpitjain.2001@gmail.com>
2022-04-13 11:34:04 +0530

    created structure

---
+++ algorithms.h
@@ -0,0 +1,10 @@
+#include "algorithms/fibonacci.h"
+#include "algorithms/huffman.h"
+#include "algorithms/lz77.h"
+#include "algorithms/range.h"
+#include "algorithms/shannon_fano.h"
+
+namespace algorithms
+{
+
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
+#include "algorithms.h"
 using namespace std;
 
 int main()
 {
-    
+    /*
+    Molshree                Huffman
+    Arpit                   Range coding
+    Yathansh                LZ77
+    Simran                  Fibonacci        
+    Nishttha                Shannon Fano 
+    */
+
 }
